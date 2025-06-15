0b53c078e8ed96dea7dcd859550f24c512a06e5f
Matt Lacey <matt@laceysnr.com>
2025-06-15 11:08:34 +0100

    Adds externs to avoid 'already defined' symbol issues in the linker

--- src/codegen/GenArith.h
+++ src/codegen/GenArith.h
@@ -12,48 +12,48 @@ Author:     M.Buras (sqward)
 #include <asm_types.h>
 #include <CodeUtils.h>
 
-int	const add_patterns[3];
-int	const sub_patterns[3];
-int	const cmp_patterns[3];
-int const and_patterns[3];
-int const eor_patterns[3];
-int const or_patterns[3];
-int const asl_patterns[3];
-int const asr_patterns[3];
-int const lsl_patterns[3];
-int const lsr_patterns[3];
+extern int	const add_patterns[3];
+extern int	const sub_patterns[3];
+extern int	const cmp_patterns[3];
+extern int const and_patterns[3];
+extern int const eor_patterns[3];
+extern int const or_patterns[3];
+extern int const asl_patterns[3];
+extern int const asr_patterns[3];
+extern int const lsl_patterns[3];
+extern int const lsr_patterns[3];
 
-int const incdec_patterns[2];
+extern int const incdec_patterns[2];
 
-uint const mpy_pattern[4];
-uint const macr_pattern[3];
-uint const mpyr_pattern[3];
-uint const max_pattern[2];
-uint const mac_pattern[4];
+extern uint const mpy_pattern[4];
+extern uint const macr_pattern[3];
+extern uint const mpyr_pattern[3];
+extern uint const max_pattern[2];
+extern uint const mac_pattern[4];
 
-void GenDiv(uint src_reg, uint dst_reg);
-void GenDMac(uint modifier, uint plusminus, int val, uint dest_reg);
-void GenAndEorOr(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
-void GenAdcSbc(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
-void GenAddSub(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
-void GenCmp(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
-void GenAddSubEorOrLong(uint insn_patt, int val, uint dest_reg);
-void GenAddSubEorOrShort(uint insn_patt, int val, uint dest_reg);
-void GenAddxSubx(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
-void GenAndiOri(uint insn_patt, int val, uint dest_reg);
-void GenAsxImmediate(uint insn_patt, int val, uint src_reg, uint dest_reg);
-void GenAsxReg(uint insn_patt, int val_reg, uint src_reg, uint dest_reg);
-void GenCmpm(uint src_reg, uint dest_reg, bcode *par_move);
-void GenCmpu(uint src_reg, uint dest_reg);
-void GenIncDec(uint inst_patt, uint reg);
-void GenLsxReg(const int *insn_patt, int src_reg, uint dest_reg);
-void GenLsxImmediate(const int *insn_patt, int val, uint dest_reg);
-void GenMul1(const uint *insn_patt, uint plusminus, uint reg_pair, uint dest_reg, bcode *par_move);
-void GenMul2(const uint *insn_patt, uint plusminus, uint src_reg, int val, uint dest_reg);
-void GenMuli(const uint *insn_patt, uint plusminus, int val, uint src_reg, uint dest_reg);
-void GenMulxx(const uint *insn_patt, uint code, uint plusminus, uint reg_pair, uint dest_reg);
-void GenMax(uint insn_patt, uint src_reg, uint dst_reg, bcode *par_move);
-void GenNorm(uint src_reg, uint dst_reg);
-void GenNormf(uint src_reg, uint dst_reg);
+extern void GenDiv(uint src_reg, uint dst_reg);
+extern void GenDMac(uint modifier, uint plusminus, int val, uint dest_reg);
+extern void GenAndEorOr(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenAdcSbc(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenAddSub(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenCmp(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenAddSubEorOrLong(uint insn_patt, int val, uint dest_reg);
+extern void GenAddSubEorOrShort(uint insn_patt, int val, uint dest_reg);
+extern void GenAddxSubx(uint insn_patt, uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenAndiOri(uint insn_patt, int val, uint dest_reg);
+extern void GenAsxImmediate(uint insn_patt, int val, uint src_reg, uint dest_reg);
+extern void GenAsxReg(uint insn_patt, int val_reg, uint src_reg, uint dest_reg);
+extern void GenCmpm(uint src_reg, uint dest_reg, bcode *par_move);
+extern void GenCmpu(uint src_reg, uint dest_reg);
+extern void GenIncDec(uint inst_patt, uint reg);
+extern void GenLsxReg(const int *insn_patt, int src_reg, uint dest_reg);
+extern void GenLsxImmediate(const int *insn_patt, int val, uint dest_reg);
+extern void GenMul1(const uint *insn_patt, uint plusminus, uint reg_pair, uint dest_reg, bcode *par_move);
+extern void GenMul2(const uint *insn_patt, uint plusminus, uint src_reg, int val, uint dest_reg);
+extern void GenMuli(const uint *insn_patt, uint plusminus, int val, uint src_reg, uint dest_reg);
+extern void GenMulxx(const uint *insn_patt, uint code, uint plusminus, uint reg_pair, uint dest_reg);
+extern void GenMax(uint insn_patt, uint src_reg, uint dst_reg, bcode *par_move);
+extern void GenNorm(uint src_reg, uint dst_reg);
+extern void GenNormf(uint src_reg, uint dst_reg);
 
 #endif /* _GENARITH_H_ */
